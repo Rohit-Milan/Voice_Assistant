591e102460740e26393ebe5b37b064e0e1c81ac3
Rohit <rohitmilan2018@gmail.com>
2023-07-27 02:07:45 +0530

    forgot to save in last step

--- V.1.0.py
+++ V.1.0.py
@@ -9,5 +9,9 @@ voices = None
 #Sound engine initialisation
 
 engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+print(type(engine))
+print(type(voices))
 
 
